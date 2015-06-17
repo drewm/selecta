bbc19c1e3496d8a229004f28efb9c509d0633eca
drewm <drew.mclellan@gmail.com>
2015-06-17 18:27:47 +0100

    More extreme tests

--- tests/ExtremeTest.php
+++ tests/ExtremeTest.php
@@ -6,8 +6,8 @@ class ExtremeTest extends PHPUnit_Framework_TestCase
 {
 	public function testBigSelector()
 	{
-		$result = Selecta::build('ul#list.mr-list li[required] div.foo.bar input[type=radio]');
-		$this->assertEquals('<ul id="list" class="mr-list"><li required><div class="foo bar"><input type="radio"></div></li></ul>', $result);
+		$result = Selecta::build('ul#list.mr-list li[required] div.foo.bar input[name=hell][type=radio][value=yes][checked][required]');
+		$this->assertEquals('<ul id="list" class="mr-list"><li required><div class="foo bar"><input name="hell" type="radio" value="yes" checked required></div></li></ul>', $result);
 	}
 
 
